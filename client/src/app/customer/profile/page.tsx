5d747a22748733e15b6a6298ff08029f537d7c17
srajan <kumarsrajan02@gmail.com>
2024-03-08 12:12:13 +0530

    fetch details works :)))

---
+++ client/src/app/customer/profile/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
+
+const CustomerInfo = () => {
+  const [customerInfo, setCustomerInfo] = useState<any>({});
+  const [cookies] = useCookies(["token"]);
+
+  useEffect(() => {
+    const fetchCustomerInfo = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/customer/info", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${cookies.token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch customer information");
+        }
+        const data = await response.json();
+        setCustomerInfo(data);
+      } catch (error) {
+        console.error("Error fetching customer information:", error);
+      }
+    };
+
+    // Fetch customer information only if token is available in cookies
+    if (cookies.token) {
+      fetchCustomerInfo();
+    }
+  }, [cookies.token]);
+
+  return (
+    <div>
+      <h2>Customer Information</h2>
+      {customerInfo && (
+        <div>
+          <p>ID: {customerInfo.customer_id}</p>
+          <p>Email: {customerInfo.email}</p>
+          <p>Role: {customerInfo.role}</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CustomerInfo;
