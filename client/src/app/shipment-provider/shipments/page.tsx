0d49099fea0f855a0bec8625c68d0de700c7ae03
srajan <kumarsrajan02@gmail.com>
2024-03-20 12:01:50 +0530

    fix: date formating

--- client/src/app/shipment-provider/shipments/page.tsx
+++ client/src/app/shipment-provider/shipments/page.tsx
@@ -110,11 +110,12 @@ export default function Shipments() {
     }
   };
 
+  // Function to format date in "dd-mm-yyyy" format
   const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
-    const day = String(date.getUTCDate()).padStart(2, "0");
-    const month = String(date.getUTCMonth() + 1).padStart(2, "0");
-    const year = date.getUTCFullYear();
+    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const year = date.getFullYear();
     return `${day}-${month}-${year}`;
   };
 
